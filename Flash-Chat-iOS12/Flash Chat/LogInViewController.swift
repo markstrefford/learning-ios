3b31e236c229eeef8a205110e7022f47a0e9a453
Mark Strefford <markstrefford@gmail.com>
2019-07-07 21:40:38 +0100

    User auth

--- Flash-Chat-iOS12/Flash Chat/LogInViewController.swift
+++ Flash-Chat-iOS12/Flash Chat/LogInViewController.swift
@@ -6,6 +6,7 @@
 
 
 import UIKit
+import Firebase
 
 
 class LogInViewController: UIViewController {
@@ -25,10 +26,18 @@ class LogInViewController: UIViewController {
 
    
     @IBAction func logInPressed(_ sender: AnyObject) {
-
+        
         
         //TODO: Log in the user
-        
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (user, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Login was successful for user \(user!)")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
     }
     
